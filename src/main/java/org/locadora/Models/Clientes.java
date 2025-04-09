63246e7e623e6b958ad2feae5e42029d469632cd
Ismael <ismael2003ho@gmail.com>
2025-04-09 16:51:58 -0300

    Add classe clientes e ListaClientes

---
+++ src/main/java/org/locadora/Models/Clientes.java
@@ -0,0 +1,52 @@
+package org.locadora.Models;
+
+public class Clientes {
+    private String nome, cpf, cnh, telefone;
+
+    public Clientes(String nome, String cpf, String cnh, String telefone) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.cnh = cnh;
+        this.telefone = telefone;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public String getCpf() {
+        return cpf;
+    }
+    public String getCnh() {
+        return cnh;
+    }
+    public String getTelefone() {
+        return telefone;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+    public void setCnh(String cnh) {
+        this.cnh = cnh;
+    }
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj instanceof Clientes) {
+            Clientes cliente = (Clientes) obj;
+            return this.cpf.equals(cliente.getCpf());
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "Nome: " + nome + "\nCPF: " + cpf + "\nCNH: " + cnh + "\nTelefone: " + telefone;
+    }
+}
+
