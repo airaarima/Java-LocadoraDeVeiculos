870bd6a2037f2336e2dd6ad8895034a3e7508d98
airaarima <arimacoelhoaira@gmail.com>
2025-04-10 23:21:10 -0300

    implementa menu

--- src/main/java/org/locadora/Main.java
+++ src/main/java/org/locadora/Main.java
@@ -1,11 +1,64 @@
 package org.locadora;
 
-import org.locadora.Models.Clientes;
-import org.locadora.Models.ListaClientes;
+import org.locadora.Menus.MenuCategoria;
+import org.locadora.Models.CategoriaLista;
+
+import java.util.Scanner;
 
 public class Main {
+    private static Scanner scanner = new Scanner(System.in);
+
+    // Cria as listas de cada classe
+    private static CategoriaLista categorias = new CategoriaLista();
+
+    // Cria os menus de cada classe e realiza injeção de dependências
+    private static MenuCategoria menuCategoria = new MenuCategoria(categorias);
 
     public static void main(String[] args) {
+        // Realiza a leitura dos arquivos CSV
+        carregarDadosCsv();
 
+        int opcao;
+        do {
+            System.out.println("\n===== LOCADORA DE VEÍCULOS =====");
+            System.out.println("1. Gerenciar Clientes");
+            System.out.println("2. Gerenciar Veículos");
+            System.out.println("3. Gerenciar Categorias");
+            System.out.println("4. Gerenciar Locações");
+            System.out.println("0. Sair");
+            System.out.print("Escolha uma opção: ");
+
+            opcao = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (opcao) {
+                case 1:
+//                    menuClientes();
+                    break;
+                case 2:
+//                    menuVeiculos();
+                    break;
+                case 3:
+                    menuCategoria.exibir();
+                    break;
+                case 4:
+//                    menuLocacoes();
+                    break;
+                case 0:
+                    System.out.println("Saindo do sistema...");
+                    break;
+                default:
+                    System.out.println("Opção inválida!");
+            }
+        } while (opcao != 0);
     }
+
+    private static void carregarDadosCsv() {
+        try {
+            categorias.lerCategoriasCsv();
+            System.out.println("Dados carregados com sucesso!");
+        } catch (Exception e) {
+            System.out.println("Erro ao carregar dados do arquivo CSV: " + e.getMessage());
+        }
     }
+}
---
+++ src/main/java/org/locadora/Menus/MenuCategoria.java
@@ -0,0 +1,96 @@
+package org.locadora.Menus;
+
+import org.locadora.Models.Categoria;
+import org.locadora.Models.CategoriaLista;
+
+import java.util.Scanner;
+
+public class MenuCategoria {
+    private CategoriaLista categoriasLista;
+    private Scanner scanner;
+
+    public MenuCategoria(CategoriaLista categoriasLista){
+        this.categoriasLista = categoriasLista;
+        this.scanner = new Scanner(System.in);
+    }
+
+    public void exibir(){
+        int opcao;
+        do {
+            System.out.println("\n===== MENU CATEGORIAS =====");
+            System.out.println("1. Incluir Categoria");
+            System.out.println("2. Excluir Categoria");
+            System.out.println("3. Editar Categoria");
+            System.out.println("4. Listar Categorias");
+            System.out.println("0. Voltar");
+            System.out.print("Escolha uma opção: ");
+
+            opcao = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (opcao) {
+                case 1:
+                    incluirCategoria();
+                    break;
+                case 2:
+//                    excluirCategoria();
+                    break;
+                case 3:
+                    editarCategoria();
+                    break;
+                case 4:
+                    System.out.println(categoriasLista.toString());
+                    break;
+            }
+        } while (opcao != 0);
+    }
+
+    private void incluirCategoria(){
+        System.out.println("\n--- INSERIR CATEGORIA ---");
+        System.out.print("ID: ");
+        int id = scanner.nextInt();
+        scanner.nextLine();
+        // Não permite o cadastro de novas categorias se o id já existir no sistema
+        if(categoriasLista.getById(id) != null){
+            System.out.println("Esse id já está vinculado a uma categoria existente.");
+            return;
+        }
+
+        System.out.print("Nome: ");
+        String nome = scanner.nextLine();
+        // Não permite o cadastro de novas categorias com nome vazio
+        if (nome.isEmpty()) {
+            System.out.println("Nome da categoria não pode ser vazio!");
+            return;
+        }
+
+        Categoria novaCategoria = new Categoria(id, nome);
+
+        categoriasLista.insereFim(novaCategoria);
+    }
+
+    private void editarCategoria(){
+        System.out.println("\n--- EDITAR CATEGORIA ---");
+        System.out.print("ID: ");
+        int id = scanner.nextInt();
+        scanner.nextLine();
+        // Verifica se a categoria existe no sistema
+        if(categoriasLista.getById(id) == null){
+            System.out.println("Categoria não encontrada.");
+            return;
+        }
+
+        System.out.print("Nome: ");
+        String nome = scanner.nextLine();
+        // Não permite a atualização de categorias com nome vazio
+        if (nome.isEmpty()) {
+            System.out.println("Nome da categoria não pode ser vazio!");
+            return;
+        }
+
+        Categoria atualizacaoCategoria = new Categoria(id, nome);
+
+        if(categoriasLista.editar(id, atualizacaoCategoria)) System.out.println("Categoria atualizada com sucesso!");
+        else System.out.println("Ocorreu um erro na atualização da categoria");
+    }
+}
