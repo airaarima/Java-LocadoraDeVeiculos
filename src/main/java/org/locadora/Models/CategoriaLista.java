7b3660c401533e60cf30e2a4470b151946bbba91
airaarima <arimacoelhoaira@gmail.com>
2025-04-10 23:22:46 -0300

    refatora método de busca por ID e implementa método de leitura do CSV

--- src/main/java/org/locadora/Models/CategoriaLista.java
+++ src/main/java/org/locadora/Models/CategoriaLista.java
@@ -1,5 +1,7 @@
 package org.locadora.Models;
 
+import java.io.*;
+
 public class CategoriaLista extends Lista<Categoria> {
     public boolean editar(int id, Categoria categoriaAtualizada){
         No<Categoria> editarElemento = getById(id);
@@ -11,9 +13,33 @@ public class CategoriaLista extends Lista<Categoria> {
 
     public No<Categoria> getById(int id){
         No<Categoria> encontrarElemento = getInicio();
-        while (encontrarElemento != null && encontrarElemento.getElemento().getIdentificador() == id ) {
+        while (encontrarElemento != null){
+            if(encontrarElemento.getElemento().getIdentificador() == id){
+                return encontrarElemento;
+            }
             encontrarElemento = encontrarElemento.getProximo();
         }
-        return encontrarElemento;
+        return null;
+    }
+
+    public void lerCategoriasCsv() {
+        String caminhoCsv = "src/main/java/org/locadora/Data/Categorias.csv";
+
+        try{
+            FileReader arquivoCsv = new FileReader(caminhoCsv);
+            BufferedReader br = new BufferedReader(arquivoCsv);
+            String linha;
+
+            // Pula o cabeçalho do csv
+            br.readLine();
+
+            while ((linha = br.readLine()) != null){
+                String[] dados = linha.split(";");
+                Categoria categoria = new Categoria(Integer.parseInt(dados[0]), dados[1]);
+                insereFim(categoria);
+            }
+        }catch (IOException exception){
+            System.out.println(exception);
+        }
     }
 }
