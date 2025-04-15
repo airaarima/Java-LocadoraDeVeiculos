e7ab3908d325ef279e0f64b188ba6a5e86804e56
2410200502 <anapoltronieri.aluno@unipampa.edu.br>
2025-04-15 15:42:47 -0300

    .

--- src/main/java/org/locadora/Main.java
+++ src/main/java/org/locadora/Main.java
@@ -1,24 +1,29 @@
 package org.locadora;
 
+import java.util.Scanner;
+
 import org.locadora.Menus.MenuCategoria;
 import org.locadora.Menus.MenuClientes;
 import org.locadora.Menus.MenuVeiculos;
 import org.locadora.Models.ListaCategoria;
-import org.locadora.Models.ListaVeiculo;
 import org.locadora.Models.ListaCliente;
+import org.locadora.Models.ListaVeiculo;
 import org.locadora.Validadores.Validacoes;
-
-import java.util.Scanner;
 
 public class Main {
     private static Scanner scanner = new Scanner(System.in);
-    private static ListaVeiculo veiculo = new ListaVeiculo();
+    private static ListaVeiculo veiculos = new ListaVeiculo();
     private static ListaCliente clientes = new ListaCliente();
-    private static Validacoes valid = new Validacoes(clientes, veiculo);
-    private static ListaCategoria categorias = new ListaCategoria(valid);
+    private static ListaCategoria categorias;
+    private static Validacoes valid;
+
+    static {
+        valid = new Validacoes(clientes, veiculos, categorias);
+        categorias = new ListaCategoria(valid);
+    }
     private static MenuCategoria menuCategoria = new MenuCategoria(categorias);
     private static MenuClientes menuClientes = new MenuClientes(clientes, valid);
-    private static MenuVeiculos menuVeiculos = new MenuVeiculos(veiculo);
+    private static MenuVeiculos menuVeiculos = new MenuVeiculos(veiculos, categorias);
 
     public static void main(String[] args) {
         // Realiza a leitura dos arquivos CSV
